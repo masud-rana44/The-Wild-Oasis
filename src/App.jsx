30a824155e50badc4bb5b4b24172407441afdd97
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-11 01:50:02 +0600

    connected supabase to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import GlobalStyles from "./styles/GlobalStyles";
+import { getCabins } from "./services/apiCabins";
 
 function App() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <>
       <GlobalStyles />
