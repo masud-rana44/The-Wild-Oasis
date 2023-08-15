767e343678cd7b59bf73bdfc8c744e81b3f33432
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-15 13:11:16 +0600

    implemented sorting option in cabin table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -9,6 +9,9 @@ function CabinTable() {
   const { cabins, isLoading } = useCabins();
   const [searchParams] = useSearchParams();
 
+  if (isLoading) return <Spinner />;
+
+  // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -18,7 +21,13 @@ function CabinTable() {
   if (filterValue === "no-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
 
-  if (isLoading) return <Spinner />;
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -32,7 +41,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,5 +1,23 @@
+import Select from "./Select";
+import { useSearchParams } from "react-router-dom";
+
 function SortBy({ options }) {
-  return <div>SortBy</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy") || "";
+
+  function handleChange(e) {
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <Select
+      options={options}
+      value={sortBy}
+      onChange={handleChange}
+      type="white"
+    />
+  );
 }
 
 export default SortBy;
