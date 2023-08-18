d1bfbcd6ae7c5e7afb39b9917fb88520b36a4b7f
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-18 23:49:36 +0600

    updated deleted booking

--- src/features/bookings/useDeleteBooking.js
+++ src/features/bookings/useDeleteBooking.js
@@ -3,32 +3,31 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
 import { toast } from "react-hot-toast";
 
-// export function useDeleteBooking() {
-//   const queryClient = useQueryClient();
-
-//   const { isLoading: isDeleting, mutate: deleteBooking } = useMutation({
-//     mutationFn: (bookingId) => deleteBookingApi(bookingId),
-//     onSuccess: () => {
-//       toast.success("Booking successfully deleted");
-
-//       queryClient.invalidateQueries({
-//         active: true,
-//       });
-//     },
-//     onError: (err) => {
-//       toast.error(err.message);
-//     },
-//   });
-
-//   return { isDeleting, deleteBooking };
-// }
-
-
 export function useDeleteBooking() {
-  const { mutate: deleteBooking, isLoading: isDeleting} = useMutation({
-    mutationFn: (bookingId) => deleteBooking(bookingId),
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate: deleteBooking } = useMutation({
+    mutationFn: (bookingId) => deleteBookingApi(bookingId),
     onSuccess: () => {
-      toast.success(`Booking ${}`)
-    }
-  })
+      toast.success("Booking successfully deleted");
+
+      queryClient.invalidateQueries({
+        active: true,
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { isDeleting, deleteBooking };
 }
+
+// export function useDeleteBooking() {
+//   const { mutate: deleteBooking, isLoading: isDeleting} = useMutation({
+//     mutationFn: (bookingId) => deleteBooking(bookingId),
+//     onSuccess: () => {
+//       toast.success(`Booking ${}`)
+//     }
+//   })
+// }
