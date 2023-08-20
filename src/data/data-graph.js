6435b373cdf2acbcac0d8d8bf6e3c0ceedd2667a
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-21 01:01:30 +0600

    dashboard completed

---
+++ src/data/data-graph.js
@@ -0,0 +1,117 @@
+export const startDataLight = [
+  {
+    duration: "1 night",
+    value: 0,
+    color: "#ef4444",
+  },
+  {
+    duration: "2 nights",
+    value: 0,
+    color: "#f97316",
+  },
+  {
+    duration: "3 nights",
+    value: 0,
+    color: "#eab308",
+  },
+  {
+    duration: "4-5 nights",
+    value: 0,
+    color: "#84cc16",
+  },
+  {
+    duration: "6-7 nights",
+    value: 0,
+    color: "#22c55e",
+  },
+  {
+    duration: "8-14 nights",
+    value: 0,
+    color: "#14b8a6",
+  },
+  {
+    duration: "15-21 nights",
+    value: 0,
+    color: "#3b82f6",
+  },
+  {
+    duration: "21+ nights",
+    value: 0,
+    color: "#a855f7",
+  },
+];
+
+export const startDataDark = [
+  {
+    duration: "1 night",
+    value: 0,
+    color: "#b91c1c",
+  },
+  {
+    duration: "2 nights",
+    value: 0,
+    color: "#c2410c",
+  },
+  {
+    duration: "3 nights",
+    value: 0,
+    color: "#a16207",
+  },
+  {
+    duration: "4-5 nights",
+    value: 0,
+    color: "#4d7c0f",
+  },
+  {
+    duration: "6-7 nights",
+    value: 0,
+    color: "#15803d",
+  },
+  {
+    duration: "8-14 nights",
+    value: 0,
+    color: "#0f766e",
+  },
+  {
+    duration: "15-21 nights",
+    value: 0,
+    color: "#1d4ed8",
+  },
+  {
+    duration: "21+ nights",
+    value: 0,
+    color: "#7e22ce",
+  },
+];
+
+export const fakeData = [
+  { label: "Jan 09", totalSales: 480, extrasSales: 20 },
+  { label: "Jan 10", totalSales: 580, extrasSales: 100 },
+  { label: "Jan 11", totalSales: 550, extrasSales: 150 },
+  { label: "Jan 12", totalSales: 600, extrasSales: 50 },
+  { label: "Jan 13", totalSales: 700, extrasSales: 150 },
+  { label: "Jan 14", totalSales: 800, extrasSales: 150 },
+  { label: "Jan 15", totalSales: 700, extrasSales: 200 },
+  { label: "Jan 16", totalSales: 650, extrasSales: 200 },
+  { label: "Jan 17", totalSales: 600, extrasSales: 300 },
+  { label: "Jan 18", totalSales: 550, extrasSales: 100 },
+  { label: "Jan 19", totalSales: 700, extrasSales: 100 },
+  { label: "Jan 20", totalSales: 800, extrasSales: 200 },
+  { label: "Jan 21", totalSales: 700, extrasSales: 100 },
+  { label: "Jan 22", totalSales: 810, extrasSales: 50 },
+  { label: "Jan 23", totalSales: 950, extrasSales: 250 },
+  { label: "Jan 24", totalSales: 970, extrasSales: 100 },
+  { label: "Jan 25", totalSales: 900, extrasSales: 200 },
+  { label: "Jan 26", totalSales: 950, extrasSales: 300 },
+  { label: "Jan 27", totalSales: 850, extrasSales: 200 },
+  { label: "Jan 28", totalSales: 900, extrasSales: 100 },
+  { label: "Jan 29", totalSales: 800, extrasSales: 300 },
+  { label: "Jan 30", totalSales: 950, extrasSales: 200 },
+  { label: "Jan 31", totalSales: 1100, extrasSales: 300 },
+  { label: "Feb 01", totalSales: 1200, extrasSales: 400 },
+  { label: "Feb 02", totalSales: 1250, extrasSales: 300 },
+  { label: "Feb 03", totalSales: 1400, extrasSales: 450 },
+  { label: "Feb 04", totalSales: 1500, extrasSales: 500 },
+  { label: "Feb 05", totalSales: 1400, extrasSales: 600 },
+  { label: "Feb 06", totalSales: 1450, extrasSales: 400 },
+];
