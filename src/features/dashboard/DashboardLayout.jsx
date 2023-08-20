d511c7c7088eb3d637adf22f03fb001e77ce320f
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-20 15:35:26 +0600

    start implementing dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statictis</div>
+      <div>Statictis</div>
+      <div>Statictis</div>
+      <div>Statictis</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 
 const FullPage = styled.div`
   min-height: 100vh;
-  background-color: var(--color-gray-50);
+  background-color: var(--color-grey-50);
   display: flex;
   align-items: center;
   justify-content: center;
