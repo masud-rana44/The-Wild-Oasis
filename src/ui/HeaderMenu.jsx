4b1900e2c89fa1f7a7a53c86f24470b4c4c0d196
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-20 14:25:53 +0600

    implemented dark mode

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,8 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import { HiOutlineUser } from "react-icons/hi2";
 import { styled } from "styled-components";
 
 import ButtonIcon from "./ButtonIcon";
-import { HiOutlineUser } from "react-icons/hi2";
+import DarkModeToggle from "./DarkModeToggle";
 import Logout from "../features/authentication/Logout";
 
 const StyledHeaderMenu = styled.ul`
@@ -19,6 +20,9 @@ function HeaderMenu() {
         <ButtonIcon onClick={() => navigate("/account")}>
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
